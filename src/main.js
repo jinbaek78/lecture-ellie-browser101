d49f84744e6657fa23f6992998a9ae6866278bbc
jinbaek <jinmb0602@gmail.com>
2024-03-29 12:42:35 +0900

    feat: implement toggling start and stop timer

--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 const button = document.querySelector('.playControlButton');
+const timer = document.querySelector('.timer');
+let leftSeconds = 10;
+let timerIntervalId;
 
 button.addEventListener('click', () => {
   togglePlayStopButton();
+  toggleTimer(timerIntervalId);
 });
 
 function togglePlayStopButton() {
   button.classList.toggle('play');
   button.classList.toggle('stop');
+}
+
+function toggleTimer(timerIntervalId) {
+  if (timerIntervalId == null) {
+    startTimer();
+  } else {
+    stopTimer(timerIntervalId);
+  }
+}
+
+function startTimer() {
+  timerIntervalId = setInterval(() => {
+    if (leftSeconds > 0) {
+      timer.textContent = `0:${--leftSeconds}`;
+    }
+  }, 1000);
+}
+
+function stopTimer(id) {
+  clearInterval(id);
+  timerIntervalId = null;
 }
