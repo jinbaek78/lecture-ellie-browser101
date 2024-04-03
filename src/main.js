0356aefe357dc03428e78c280858a74aae6c12e6
jinbaek <jinmb0602@gmail.com>
2024-04-03 20:59:41 +0900

    refactor: remove unused code

--- src/main.js
+++ src/main.js
@@ -11,12 +11,6 @@ const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
-const carrotSound = new Audio('./sound/carrot_pull.mp3');
-const alertSound = new Audio('./sound/alert.wav');
-const bgSound = new Audio('./sound/bg.mp3');
-const bugSound = new Audio('./sound/bug_pull.mp3');
-const windSound = new Audio('./sound/game_win.mp3');
-
 let started = false;
 let score = 0;
 let timer = undefined;
